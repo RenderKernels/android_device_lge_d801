53b2040a1a5885d097f7d86ade159e5d1b12ba67
Snuzzo <jrracinfan@gmail.com>
2015-09-25 23:28:23 -0400

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,5 @@
+ifneq ($(filter d801,$(RENDER_PRODUCT)),)
+
+include $(DEVICE_MAKEFILE)
+
+endif
---
+++ render.mk
@@ -0,0 +1,4 @@
+PRODUCT_NAME := render_d801
+PRODUCT_DEFCONFIG := d801_defconfig
+
+include device/lge/g2-common/common.mk
